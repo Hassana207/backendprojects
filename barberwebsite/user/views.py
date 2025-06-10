ea5762eaa9e4ebd3240cd2266f0b0a53cd338088
Hassan Ayinde <ayindehassan776@gmail.com>
2025-06-10 17:41:45 -0400

    login_user view is created where by authenticationform imported is use to authenticate user input and then validate and direct the user to their respective template

--- barberwebsite/user/views.py
+++ barberwebsite/user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterUser
 from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
 
@@ -10,8 +12,26 @@ def register_user(request):
         if form.is_valid():
             user = form.save()
             login(request,user )
-            return redirect("homepage")
-        else:
-            form = RegisterUser()
+            return redirect("login")
+    else:
+         form = RegisterUser()
     return render (request, "register.html",{"form":form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user is not None:
+             login(request,user)
+             if user.is_staff:
+                return redirect('adminDashBoard')
+             else :
+                return redirect("customerDashboard")
+             
+        else:
+            form= AuthenticationForm()
+    return render (request,"login.html",{"form":form})
+
 
