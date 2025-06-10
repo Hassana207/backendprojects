9fdcb6ca6badebbe3a12ca49819223928ad181a0
Hassan Ayinde <ayindehassan776@gmail.com>
2025-06-10 16:09:53 -0400

    in my forms.py changed my class name from a camelCase to PascalCase and for my views.py register_user by validating user information then redirecting them login page

--- barberwebsite/user/forms.py
+++ barberwebsite/user/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class registerUser(UserCreationForm):
+class RegisterUser(UserCreationForm):
     class meta:
        model = User
        fields = ["username","password1","password2"]
--- barberwebsite/user/views.py
+++ barberwebsite/user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterUser
+from django.contrib.auth import login
 
 # Create your views here.
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUser(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user )
+            return redirect("homepage")
+        else:
+            form = RegisterUser()
+    return render (request, "register.html",{"form":form})
+
