b7db94e0da8d8c452920ca56fb3449dacf160a8a
Hassan Ayinde <ayindehassan776@gmail.com>
2025-06-13 18:41:10 -0400

    A bookings model created and new apps created (service and barber ). The new apps are thereby added to the barberwebsite sinstalled apps.

--- barberwebsite/bookings/models.py
+++ barberwebsite/bookings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from service.models import Service
+from barbers.models import BarberInfo 
 
 # Create your models here.
+
+class Bookings(models.Model):
+    customer = models.ForeignKey(User,on_delete=models.CASCADE,related_name='bookings')
+    email = models.EmailField()
+    services = models.ManyToManyField(Service)
+    barber = models.ManyToManyField(BarberInfo)
+    date = models.DateTimeField()
+
+    def _str_ (self):
+        return f"Booking by {self.customer.username} on {self.date}"
+    
